a177bc021aa04faa53e58c3217f9e18b84848ef4
Anderson Miyada <anderson.bcc2013@gmail.com>
2018-02-14 21:59:45 +0900

    Add files via upload

--- Perceptron.java
+++ Perceptron.java
@@ -1,103 +1,93 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Bacharelado em Ciência da Computação 7ºS
+ *Anderson Miyada RA: 525626
+ *Jorge Takano Júnior RA: 529745
  */
-package xorneural;
-
-import java.util.Random;
-
+package alfabeto;
 
 /**
  *
- * @author 
- * Anderson Miyada RA 525626
- * Jorge Takano Júnior RA529745
+ * @author anderson
  */
 public class Perceptron {
-
-   double pesos[];
-   int inter_saida;
     
-   //Construtor OK!
+    double pesos[];
+    
+    //**** Cria um neurônio Perceptron e inicializa os pesos *****
     public Perceptron(int n)
     {
-      pesos = new double[n];
-       //pesos = new double[] {-0.3, 0.2, 0.0};
+        //pesos = new double[n+1];
+        int a = n+1;
         
-     Random r = new Random();
-     
-        for(int i=0; i< pesos.length; i++)
+        pesos = new double[a];
+         
+        // Insere pesos aleatórios
+        for(int i=0; i< a; i++)
         {
             //Inserindo números aleatórios positivos e nagativos exceto o 0
             if((i%2) == 0)
-            {
-            pesos[i] = r.nextDouble();
-            }
+            pesos[i] = (double)(-1 + Math.random() * 0);
                 else
-                   pesos[i] = -(r.nextDouble());
-            
+                    pesos[i] = (double)-(-1 + Math.random() * 0);
         }
-      
     }
-    
-    //Método y OK!
-    public int y(int x[])
-    {    //System.out.println("Entrou Y" );
         
-        return (soma(x) >= 0 ? 1 : 0);  
-         
+    //****** Funão de saida do neurônio *******
+    public int y(int x[])
+    {    
+        return (soma(x) >= 0) ? 1 : 0; // " ? "-> se / " : " -> senão
     }
     
-    //Método soma OK!
+    //****** Funão de somatório dos pesos de entrada  *******
     public double soma(int x[])
     {
-        //System.out.println("Entrou soma" );
-        //Armazena o valor do peso0 cuja entrada é sempre 1. 
         double u = pesos[0];
-        
+               
         for(int i=1; i < pesos.length; i++)
             {
-               
                 u += x[i-1] * pesos[i];                
             }
-        
+       
         return u;
     }
     
-    public void treinar(int padroes[][], int dj[], double eta)
+    //****** Funão de treinamento do neurônio  *******
+    public void treinar(int padroes[][], int dj[][], double eta, int posvetsaida)
     {
         boolean temerro=true;
-        int count =0;
+        int pos = posvetsaida;
+        //int count =0;
+        int erro;
         
         while(temerro)
         {
             temerro = false;
-            //System.out.println("tem erro");
+            
             for(int i=0; i<padroes.length;i++)
             {
+                //Verifica o erro de acordo com a saida y (0 ou 1)
+                //com a posição respectiva desejada da matriz dj
                 int saida = y(padroes[i]);
-                int erro = dj[i]-saida;
+                erro = dj[i][pos]- saida;
                 
                 if(erro!=0)
                 {
                     temerro = true;
-                    //System.out.println("atualiza peso");
+                    
                     // Atualiza o peso W0.
                     pesos[0] += eta * erro; 
                
                     for(int j=0; j < pesos.length-1; j++)
                     {
-                        // Atualiza o peso W1 e W2.
+                        // Atualiza o peso W1 ... W25.
                         pesos[j+1] += eta * erro * padroes[i][j]; 
-                        //System.out.println("contador"+ j);
+                        //System.out.printf("%d\n",count++);
                     }
                     
                 }
-                //System.out.println("saiu do erro");
+                
             }
             
-        }//System.out.println("treinado");
-    }//End Treinar
-        
-}//End Class
+        }
+    }
+}
